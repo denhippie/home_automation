a60b33c4db3a593752de9635d3163a8e29cf0dce
David <denhippie@gmail.com>
2018-08-25 10:44:14 +0200

    Upgrade to latest pyharmony version, and use its callback feature for changing activities.

--- home_automation.py
+++ home_automation.py
@@ -241,9 +241,10 @@ class HarmonyStateMonitor(object):
         
     def connect(self):
         logging.info("connecting harmony: %s:%s" % (self.ip, self.port))
-        self.harmony_token = pyharmony.ha_get_token(self.ip, self.port)
+        #self.harmony_token = pyharmony.ha_get_token(self.ip, self.port)
         #logging.info(harmony_token)
-        self.harmony_client = pyharmony.ha_get_client(self.harmony_token, self.ip, self.port)
+        #self.harmony_client = pyharmony.ha_get_client(self.harmony_token, self.ip, self.port)
+        self.harmony_client = pyharmony.get_client(self.ip, self.port, self.state_change_callback)
         #logging.info(harmony_client)
         self.harmony_config_cache = self.harmony_client.get_config()
         logging.info("Harmony connected. Connecting to ZMQ")
@@ -259,6 +260,11 @@ class HarmonyStateMonitor(object):
         if self.harmony_client != None:
             self.harmony_client.disconnect(send_close=True)
             
+    def state_change_callback(self, activity_id):
+        activity_name = self.get_activity_name(activity_id)
+        logging.info("State change callback [%s]" % activity_name)
+        self.process_new_state(activity_name)
+    
     def get_activity_name(self, current_activity_id):
         current_activity = ([x for x in self.harmony_config_cache['activity'] if int(x['id']) == current_activity_id][0])
         if type(current_activity) is dict:
@@ -277,34 +283,37 @@ class HarmonyStateMonitor(object):
         return self.harmony_client.get_current_activity()
     
     def get_activity(self):
-        current_activity_id = self.get_activity_safe()
-        #logging.info(current_activity_id)
-        # Use the cached config first, that takes way longer to get than the activity id.
-        activity_name = self.get_activity_name(current_activity_id)
+        return self.get_cached_activity_name(self.get_activity_safe())
+        
+    def get_cached_activity_name(self, activity_id):
+        # Use the cached config first, getting the name takes way longer to get than the activity id.
+        activity_name = self.get_activity_name(activity_id)
         if activity_name != None:
             return activity_name
         logging.info("refreshing config")
         self.harmony_config_cache = self.harmony_client.get_config()
-        return self.get_activity_name(current_activity_id)
+        return self.get_activity_name(activity_id)
     
     def power_off(self):
         logging.info("Shutting down Harmony")
         self.harmony_client.power_off()
         
     def check_state_change(self):
-        current_state = self.get_activity()
-        if current_state != self.last_state:
-            logging.info("State change: [%s] --> [%s]" % (self.last_state, current_state))
+        self.process_new_state(self.get_activity())
+        
+    def process_new_state(self, new_state):
+        if new_state != self.last_state:
+            logging.info("State change: [%s] --> [%s]" % (self.last_state, new_state))
             if self.handler_func != None:
-                self.handler_func(self.last_state, current_state)
-            self.last_state = current_state
+                self.handler_func(self.last_state, new_state)
+            self.last_state = new_state
             
     def check_zmq_event(self):
         logging.debug("Checking ZMQ event")
         try:
             message = self.zmq_socket.recv(flags=zmq.NOBLOCK)
             logging.info("Received message [%s]" % message)
-            self.check_state_change()
+            #self.check_state_change()
             self.message_handler(message)
             return True
         except:
